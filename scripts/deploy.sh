e65ccd0ee22c925973c1579ec042aff366191092
James Coller <35606193+jamescoller@users.noreply.github.com>
2025-05-25 18:27:16 -0400

    Updated deployment script with correct command

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,7 +4,7 @@ set -e
 # Trust the mounted repo location
 git config --global --add safe.directory /mnt/inventory_management
 
-cd /mnt/inventory_management/ || exit 1
+cd /volume1/docker/inventory_management || exit 1
 echo "Pulling latest code..."
 git fetch origin master
 git reset --hard origin/master
