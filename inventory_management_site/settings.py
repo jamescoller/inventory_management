24f409ade14daa1115de105d518548da6082e9a1
James Coller <35606193+jamescoller@users.noreply.github.com>
2025-06-21 16:21:24 -0400

    Added comma formatting in the "total value" view on the dashboard.

--- inventory_management_site/settings.py
+++ inventory_management_site/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.humanize",
     "django_tables2",
     "django_filters",
     "django_htmx",
