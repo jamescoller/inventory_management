74c36eb3ab7ca8037de2823af843e9f2512f5db2
James Coller <35606193+jamescoller@users.noreply.github.com>
2025-08-01 16:36:26 -0400

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,6 +16,14 @@ echo "Pulling latest code..."
 git fetch origin master
 git reset --hard origin/master
 
+# deploy.sh
+
+ENV_DEST="./.env"
+ENV_SOURCE="/home/runner/.env_shared"
+
+echo "Linking .env from shared volume"
+ln -sf "$ENV_SOURCE" "$ENV_DEST"
+
 echo "Restarting Docker Compose stack..."
 docker-compose down
 docker-compose up -d --build
