1ea6f5c6410bccc6ceb177c77ff20d2bfdab1494
James Coller <35606193+jamescoller@users.noreply.github.com>
2025-08-01 13:17:05 -0400

    Added apple icons and a favicon. Additionally, updated the gunicon configuration to launch multiple workers and threads to improve performance.

--- inventory_management_site/settings.py
+++ inventory_management_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import logging
 import os
 from os.path import dirname, join
 from pathlib import Path
